84ca68a70ac4ae858f95c8f24bc81009e3f30a6e
gentrainor <143761797+gentrainor@users.noreply.github.com>
2025-04-07 18:45:55 -0400

    Add backend Flask app with sensor endpoint

--- envirocube-ui/src/components/Account.js
+++ envirocube-ui/src/components/Account.js
@@ -13,7 +13,7 @@ function Account() {
           This object visually represents these data points through color changes to allow people to understand their 
           surroundings better. 
           EnviroCube Web is a cutting-edge web application designed to help users monitor and track local environmental 
-          data through out EnviroCube interactive object. 
+          data through our EnviroCube interactive object. 
           With features like real-time weather updates, air quality alerts, and data tracking, 
           EnviroCube provides valuable insights to help users make informed decisions about their environment.
         </p>
